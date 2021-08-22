9c3ba27dbd92bd9bbd8bd51534ee74f7517169d3
Jeet Bhowmik <jeetbhowm@gmail.com>
2021-08-21 17:39:19 -0700

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,9 +1,10 @@
 class Calculator {
-
     constructor(display, buttons, symbolMap) {
         this.display = display;
         this.buttons = buttons;
         this.symbolMap = symbolMap; 
+
+        this.display.addEventListener('input', this.processKeyboard);
 
         for (const button of buttons) {
             switch (button.textContent) {
@@ -22,29 +23,79 @@ class Calculator {
         }
     }
 
+    // Add something to the end of the display.
     append(char) {
-        display.value += char; 
-    }
-
-    evaluate() {
-        if (display.value === "") {
+        if (currentState === State.INACTIVE) {
             return; 
         }
 
-        // Replaces calculator symbols with JavaScript operators. 
-        for (const [symbol, target] of symbolMap) {
-            let replacement = display.value.replace(symbol, target);
-            display.value = replacement; 
+        if (char === ".") {
+            if (display.value.includes(".")) {
+                return; 
+            }
         }
-    
-        display.value = eval(display.value);
+
+        if (hasFinishedCalculation) {
+            if (char >= 0 && char <= 9) {
+                display.value = "";
+            }
+        }
+
+        if (display.value === "" && symbolMap.get(char)) {
+            display.value = "0";
+        }
+
+        hasFinishedCalculation = false; 
+        display.value += char;
     }
 
-    del() {
+    // Process they keyboard input.
+    processKeyboard() {
+        // No suppport for this yet. For now, delete the input. 
         display.value = display.value.substring(0, display.value.length - 1);
     }
 
+    // Evaluate the expression that is currently on the display. 
+    evaluate() {
+        if (display.value === "" || currentState === State.INACTIVE) {
+            return; 
+        } 
+ 
+        for (const [symbol, target] of symbolMap) {
+            // A copy of the display with calculator symbols replaced by JavaScript operators. 
+            let replacementString = display.value.replaceAll(symbol, target); 
+            display.value = replacementString; 
+        }
+        
+        try {
+            display.value = eval(display.value);
+        } catch {
+            display.value = "Error";
+            display.style.backgroundColor = "red";
+            display.style.color = "white";
+            currentState = State.INACTIVE; 
+        }
+
+        hasFinishedCalculation = true; 
+    }
+
+    // Remove the character at the end of the display. 
+    del() {
+        if (currentState === State.ACTIVE) {
+            display.value = display.value.substring(0, display.value.length - 1);
+        } else {
+            return; 
+        }
+    }
+
+    // Remove everything on the display. 
     clear() {
         display.value = '';
+
+        if (currentState === State.INACTIVE) {
+            display.style.backgroundColor = "#2c8f2c";
+            display.style.color = "#91f2b2";
+            currentState = State.ACTIVE; 
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+let State = {
+    INACTIVE: 0,
+    ACTIVE: 1, 
+}
+
+let currentState = State.ACTIVE; 
+let hasFinishedCalculation = false; 
+let hasDecimalPoint = false; 
+
+
 const symbolMap = new Map(); 
 
 symbolMap.set(Symbol.MULTIPLY, "*");         
 symbolMap.set(Symbol.DIVIDE, "/");         
 symbolMap.set(Symbol.ADD, "+");        
-symbolMap.set(Symbol.SUBTRACT, "-");  
+symbolMap.set(Symbol.SUBTRACT, "-");
+symbolMap.set(".", ".");
+
 
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector("input");
